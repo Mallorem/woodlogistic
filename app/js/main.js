47d2d7fd732ae5cab10c949d20f1674549df965e
Olga Poddubskaya <ocoolane@gmail.com>
2021-08-27 00:11:34 +0800

    script for tabs was added

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,49 @@
 if (document.readyState === 'loading') {
 
     document.addEventListener("DOMContentLoaded", function (event) {
+        let aboutTabItem = document.querySelector(".tabs__item[href='#about']");
 
-        // menu 
-        const toggleActiveClasses = () => {
-            document.body.classList.toggle('page_lock');
-            iconMenu.classList.toggle('menu__icon_active');
-            menuBody.classList.toggle('menu__body_active');
+        const tabs = document.querySelectorAll(".tabs__item");
+        const showTab = function (event, tabId, activeClass, currentClass) {
+            console.log(aboutTabItem);
+            event.preventDefault();
+            const activeTabBody = document.querySelector(`.${activeClass}`);
+            activeTabBody.classList.remove(activeClass);
+            const tab = document.querySelector(`.${currentClass}${tabId}`);
+            tab.classList.add(activeClass);
+        };
+
+        const switchLink = (link, activeClass) => {
+            const activeTab = document.querySelector(`.${activeClass}`);
+            activeTab.classList.remove(activeClass);
+            link.classList.add(activeClass);
+
         }
 
-        const iconMenu = document.querySelector('.menu__icon');
-        const menuBody = document.querySelector('.menu__body');
+        for (let i = 0; i < tabs.length; i++) {
+            let tab = tabs[i];
+            tab.addEventListener('click', function (e) {
+                console.log(tabs);
+                const id = tab.getAttribute('href');
+                showTab(e, id, 'tabs__block_active', 'tabs__block');
+                switchLink(e.currentTarget, 'tabs__item_active')
+            })
+        }
 
-        if (iconMenu) iconMenu.addEventListener('click', toggleActiveClasses)
+        // Dispatch the event.
+        aboutTabItem.click();
+
+        // menu 
+        // const toggleActiveClasses = () => {
+        //     document.body.classList.toggle('page_lock');
+        //     iconMenu.classList.toggle('menu__icon_active');
+        //     menuBody.classList.toggle('menu__body_active');
+        // }
+
+        // const iconMenu = document.querySelector('.menu__icon');
+        // const menuBody = document.querySelector('.menu__body');
+
+        // if (iconMenu) iconMenu.addEventListener('click', toggleActiveClasses)
 
     });
 }
